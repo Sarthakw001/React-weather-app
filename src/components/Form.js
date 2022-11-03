8b7ac345215f3c9f722642c67b88f3c53cc37fb7
Sarthakw001 <sarthakw01@gmail.com>
2022-11-03 17:14:55 +0530

    Initial Commit

---
+++ src/components/Form.js
@@ -0,0 +1,29 @@
+import "./form.css";
+
+const Form = ({ handleChange, getWeather }) => {
+  return (
+    <div className="form">
+      <form>
+        <input
+          type="text"
+          name="city"
+          placeholder="city"
+          onChange={handleChange}
+          required
+        ></input>
+        <input
+          name="country"
+          type="text"
+          placeholder="country"
+          onChange={handleChange}
+          required
+        ></input>
+        <button className="getweather" onClick={getWeather}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
